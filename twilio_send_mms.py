8c50957e71f2f53e7031362ed599d318605bb710
junaidtariq <junaidtariq166@gmail.com>
2019-07-16 17:29:13 +0500

    twilio integration

---
+++ twilio_send_mms.py
@@ -0,0 +1,26 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+from twilio.twiml.messaging_response import MessagingResponse
+
+account_sid = ''
+auth_token = ''
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+         body='This is the ship that made the Kessel Run in fourteen parsecs?',
+         from_='',
+         media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg'],
+         to=''
+     )
+
+print(message.sid)
+
+def sms_reply():
+    """Respond to incoming with a simple text message."""
+
+    resp = MessagingResponse()
+    print(resp)

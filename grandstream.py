6f80e7864bc8d6ab7d60e87e7b6c7a6adcf86c3d
Juniad522 <junaidtariq166@gmail.com>
2020-10-13 14:46:56 +0500

    :relieved: grandstream progess

--- grandstream.py
+++ grandstream.py
@@ -9,8 +9,6 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 username = "cdrapi"
 password = "cdrapi123B2C3Ni"
-
-api_cookie = ""
 req_headers = {"Content-Type": "application/json"}
 
 
@@ -26,34 +24,31 @@ def login(url):
         }
     }
     res = requests.post(url, data=json.dumps(data), headers=req_headers, verify=False)
+
     if res.status_code == 200:
         print(res.content)
     if res.status_code == 200:
         r = json.loads(res.text)
         if r['status'] == 0:
             challenge = r['response']['challenge']
-            print(challenge)
-            print(password)
             p = challenge + password
-            print(p)
             token = hashlib.md5(p.encode('utf-8')).hexdigest()
             print(token)
             data2 = {
                 "request": {
                     "action": "login",
                     "token": token,
-                    "url": "https://72.137.146.94:8089",
+                    "url": "https://72.137.146.94:8089/api",
                     "user": username
                 }
             }
-            print(json.dumps(data2))
             res = requests.post(url, data=json.dumps(data2), headers=req_headers, verify=False)
             if res.status_code == 200:
                 r = json.loads(res.text)
                 if r['status'] == 0:
                     api_cookie = r['response']['cookie']
                     print('API Cookie is => {0}'.format(api_cookie))
-                    return True
+                    return api_cookie
                 else:
                     print('Login: Status => {0}'.format(r['status']))
                     print(r)
@@ -64,11 +59,20 @@ def login(url):
     return False
 
 
-def getSystemGeneralStatus(url):
+def getSystemGeneralStatus(url, api_cookie):
     # data = {
     #     "request": {
     #         "action": "getSystemGeneralStatus",
     #         "cookie": api_cookie
+    #     }
+    # }
+    # data = {
+    #     "request": {
+    #         "action": "listAccount",
+    #         "cookie": api_cookie,
+    #         "item_num": "30", "options": "extension,account_type,fullname,status,addr", "page": "1",
+    #         "sidx": "extension",
+    #         "sord": "asc"
     #     }
     # }
     data = {
@@ -79,10 +83,11 @@ def getSystemGeneralStatus(url):
         }
     }
     res = requests.post(url, data=json.dumps(data), headers=req_headers, verify=False)
+    print(res.text)
     if res.status_code == 200:
         r = json.loads(res.text)
         if r['status'] == 0:
-            print(res.content['response'])
+            print(res.content)
             print(r)
         else:
             print('getSystemGeneralStatus: Status => {0}'.format(r['status']))
@@ -92,9 +97,10 @@ def getSystemGeneralStatus(url):
 
 
 if __name__ == '__main__':
-    url = 'http://72.137.146.94:8089/api'
-    if login(url):
-        getSystemGeneralStatus(url)
+    url = 'https://72.137.146.94:8089/api'
+    api_cookie = login(url)
+    if api_cookie:
+        getSystemGeneralStatus(url, api_cookie)
     # get_cdr_logs()
     # get_cdr_logs_incoming()
     # get_cdr_logs_incoming_voice_mails()
